d3bc3c8cb7e0ba4005bc657c6b822725147f4213
YJ <yjciacia@gmail.com>
2016-02-02 16:03:35 +0800

    2016-02-02

--- src/main/java/com/spring/dao/UserDao.java
+++ src/main/java/com/spring/dao/UserDao.java
@@ -1,10 +1,13 @@
 package com.spring.dao;
 
 import com.spring.model.User;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by YanJun on 2016/2/2.
  */
+
+@Repository
 public interface UserDao {
     User findUserByEmail(String email);
 }
